03af99fc4375a46edc132e8ad4fe3dd928543f91
hfcorriez <hfcorriez@gmail.com>
2013-04-29 22:05:24 +0800

    Add php unit test

---
+++ tests/UtilsTest.php
@@ -0,0 +1,66 @@
+<?php
+
+use Croon\Utils;
+
+class UtilsTest extends PHPUnit_Framework_TestCase
+{
+    public function testFormat()
+    {
+        $ret = Utils::parseLine('* * * * * * pwd');
+        $this->assertEquals('* * * * * * ', $ret[0]);
+        $this->assertEquals('pwd', $ret[1]);
+    }
+
+    public function testCompatibleFormat()
+    {
+        $ret = Utils::parseLine('* * * * * pwd');
+        $this->assertEquals('0 * * * * * ', $ret[0]);
+        $this->assertEquals('pwd', $ret[1]);
+    }
+
+    public function testRule()
+    {
+        $ret = Utils::checkRule('* * * * * *');
+
+        $this->assertTrue($ret);
+    }
+
+    public function testRange()
+    {
+        $ret = Utils::checkIndexRange(0, 30);
+        $this->assertTrue($ret);
+
+        $ret = Utils::checkIndexRange(0, 61);
+        $this->assertFalse($ret);
+
+        $ret = Utils::checkIndexRange(1, 30);
+        $this->assertTrue($ret);
+
+        $ret = Utils::checkIndexRange(1, 61);
+        $this->assertFalse($ret);
+
+        $ret = Utils::checkIndexRange(2, 2);
+        $this->assertTrue($ret);
+
+        $ret = Utils::checkIndexRange(2, 25);
+        $this->assertFalse($ret);
+
+        $ret = Utils::checkIndexRange(3, 1);
+        $this->assertTrue($ret);
+
+        $ret = Utils::checkIndexRange(3, 32);
+        $this->assertFalse($ret);
+
+        $ret = Utils::checkIndexRange(4, 1);
+        $this->assertTrue($ret);
+
+        $ret = Utils::checkIndexRange(4, 13);
+        $this->assertFalse($ret);
+
+        $ret = Utils::checkIndexRange(5, 1);
+        $this->assertTrue($ret);
+
+        $ret = Utils::checkIndexRange(5, 8);
+        $this->assertFalse($ret);
+    }
+}
